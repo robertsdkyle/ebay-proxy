9e0894603a7b541fc3080ac5a7949c8a22f6e855
waywardtraderz <waywardtraderz@gmail.com>
2026-05-09 10:25:58 -0400

    Update ebay.js

--- netlify/functions/ebay.js
+++ netlify/functions/ebay.js
@@ -1,4 +1,3 @@
-// netlify/functions/ebay.js
 exports.handler = async function () {
     const APP_ID = "KyleRobe-Website-PRD-309adb704-bac68a48";
     const STORE_NAME = "waywardtraderz";
@@ -15,24 +14,13 @@ exports.handler = async function () {
     const response = await fetch(url);
     const data = await response.json();
 
-    const items =
-        data?.findItemsIneBayStoresResponse?.[0]
-            ?.searchResult?.[0]?.item || [];
-
-    const listings = items.map((item) => ({
-        id:    item.itemId[0],
-        title: item.title[0],
-        price: item.sellingStatus[0].currentPrice[0].__value__,
-        image: item.galleryURL?.[0] || "",
-        url:   item.viewItemURL[0],
-    }));
-
+    // Return the raw eBay response so we can see what's happening
     return {
         statusCode: 200,
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(listings),
+        body: JSON.stringify(data),
     };
 };
